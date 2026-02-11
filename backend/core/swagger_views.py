58bea94bc8a00b2aac0104540048fe28083ab373
meliouar16 <melissa.aitouarab@ynov.com>
2026-02-11 11:05:27 +0100

    Fix SonarQube: ajout @require_GET sur swagger views

--- backend/core/swagger_views.py
+++ backend/core/swagger_views.py
@@ -4,8 +4,10 @@ import json
 from django.conf import settings
 from django.http import JsonResponse, HttpResponse
 from django.template import loader
+from django.views.decorators.http import require_GET
 
 
+@require_GET
 def openapi_json(request):
     spec_path = Path(settings.BASE_DIR) / "OpenAPISpec.json"
     with spec_path.open("r", encoding="utf-8") as f:
@@ -13,6 +15,7 @@ def openapi_json(request):
     return JsonResponse(data)
 
 
+@require_GET
 def swagger_ui(request):
     template = loader.get_template("swagger_ui.html")
     return HttpResponse(template.render({"schema_url": "/swagger/openapi.json"}, request))
